470bb058b9671fa6e7076af41bf413a589c870dc
saramahmoud55 <saraelsadek55555@gmail.com>
2022-01-03 21:11:07 +0200

    uploading jsx task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{border:"solid 1px black;max-width:100vw"}}>
+      <h1 class="title red">Your name here</h1>
+      <br />
+      <img src={imageInSrc} alt="Baseet" className='imageStyle' />
+      <br />
+      <img src="/imageInPublic.jpg" alt="Shafik Habar" className='imageStyle' />
+
+      </div>
+      <video width="320" height="240" controls>
+        <source src="/shafikVedio.mp4" type="video/mp4" />
+       </video>
       </header>
     </div>
   );
